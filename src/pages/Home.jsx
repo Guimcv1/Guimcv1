dd20e36130415841a2ba5eb193f15f1f5ae1e48f
Guilherme <guimcvilmar@gmail.com>
2026-02-12 15:13:06 -0300

    Odeio o react

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import FloatingLines from '/src/components/FloatingLines.jsx';
 
-//Não ta funcionando essa bomba eu odeio isso
+//Não ta funcionando
 const App = () => {
     return (
         <div className='flex w-screen h-screen'>
