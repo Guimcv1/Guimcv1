a20523727d35eea42eba13fea598c9ff0c2c6646
Guilherme <guimcvilmar@gmail.com>
2026-02-12 15:07:30 -0300

    teste 5550

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 // Importe suas páginas
-import Home from './pages/Home';
+import Home from '/src/pages/Home.jsx';
 
 
 function App() {
