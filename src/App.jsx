09584cfb8f7dc4a45a7f0ab1c4d01b4ea05b155a
Guilherme <guimcvilmar@gmail.com>
2026-02-12 15:26:46 -0300

    Fix routing for GitHub Pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
 // src/App.jsx
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Home from '/src/pages/Home.jsx';
-
+import Home from "/src/pages/Home.jsx";
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/Guimcv1">
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="*" element={<h1 style={{color:'black'}}>Página não encontrada</h1>} />
+        <Route
+          path="*"
+          element={<h1 style={{ color: "black" }}>Página não encontrada</h1>}
+        />
       </Routes>
     </BrowserRouter>
   );
